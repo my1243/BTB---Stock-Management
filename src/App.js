a0e8da869c1261b4b690d9c63d8d8b5d5649f4f5
Tejpatel1806 <tejpatel1806@gmail.com>
2022-08-13 12:13:34 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import logo from './logo.svg';
-import "./index.css";
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Home1, {cmp} from "./components/after/Home1";
+import Hello from "./components/after/Hello";
+import Fifth from "./components/Home/Fifth";
+import  Model3 from "./components/Home/Model3";
 
-function App() {
+const App = () => {
+
+
+
   return (
-    <h1 className='text-red-400 font-bold text-3xl'>hello world</h1>
+    <>
+      <Router>
+        <Routes>
+            <Route path="/portfolio" element={<Home1/>} />
+          <Route
+            element={
+              <>
+                <Navbar />
+                <Outlet />
+                <Footer />
+              </>
+            }
+          >
+            <Route path="/" element={<Home />} />
+            <Route path="/after" element={<Home1 />} />
+            <Route path="/after1" element={<Hello />} />
+            <Route path="/model" element={<Model3 />} />
+
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
