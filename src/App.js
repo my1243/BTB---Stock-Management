d586a8c8cc3aaf6744a5956787be997976733e91
my1243 <mihiryarra@gmail.com>
2022-08-14 00:37:37 +0530

    added route for compare and todo

--- src/App.js
+++ src/App.js
@@ -11,8 +11,10 @@ import Home from "./components/Home";
 import Home1, {cmp} from "./components/after/Home1";
 import Hello from "./components/after/Hello";
 import Fifth from "./components/Home/Fifth";
-import  Model3 from "./components/Home/Model3";
-
+import  Model3 from "./components/after/Model3";
+import Fourth from "./components/Home/Fourth";
+import Third from "./components/Home/Third";
+import Compare from "./components/after/Compare";
 
 const App = () => {
 
@@ -23,6 +25,8 @@ const App = () => {
       <Router>
         <Routes>
             <Route path="/portfolio" element={<Home1/>} />
+              <Route path="/portfolio/compare" element={<Compare/>} />
+              <Route path="/portfolio/addStock" element={<Model3/>} />
           <Route
             element={
               <>
@@ -33,9 +37,9 @@ const App = () => {
             }
           >
             <Route path="/" element={<Home />} />
-            <Route path="/after" element={<Home1 />} />
-            <Route path="/after1" element={<Hello />} />
-            <Route path="/model" element={<Model3 />} />
+            {/* <Route path="/model" element={<Model3 />} /> */}
+            {/* <Route path="/fourth" element={<Fourth />} />
+            <Route path="/third" element={<Third />} /> */}
 
           </Route>
         </Routes>
