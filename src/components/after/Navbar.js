9f0194e823a8af0705d5e5031689e8717051b739
dobariyapurva2003 <purvadobariya9@gmail.com>
2022-08-14 08:01:27 +0530

    Add logout button

--- src/components/after/Navbar.js
+++ src/components/after/Navbar.js
@@ -3,21 +3,22 @@ import { Link } from "react-router-dom";
 
 const Navbar1 = () => {
     return (
-        <div className="flex flex-row justify-between px-4 items-center sticky top-0 bg-white">
-            <div>
-                <Link to="/portfolio"><h1 className="text-2xl font-bold uppercase">Portfolio</h1></Link>
-            </div>
-            <div className="flex flex-row items-center justify-center">
-                <div className="h-12 w-12 rounded-full overflow-hidden m-2"><img src="../avatar.jpg" /></div>
+        
+            <div className="flex flex-row justify-between px-4 items-center sticky top-0 bg-white">
                 <div>
-                    <h1 className="font-medium text-md">Barly Vallendito</h1>
-                    <h3 className="font-medium text-slate-600 text-sm">barleyvallen@gmail.com</h3>
+                    <Link to="/portfolio"><h1 className="text-2xl font-bold uppercase">Portfolio</h1></Link>
                 </div>
-                <div>
-                    <a className="">Log Out</a>
+                <div className="flex flex-row items-center justify-center">
+                    <div className="h-12 w-12 rounded-full overflow-hidden m-2"><img src="../avatar.jpg" /></div>
+                    <div>
+                        <h1 className="font-medium text-md">Barly Vallendito</h1>
+                        <h3 className="font-medium text-slate-600 text-sm">barleyvallen@gmail.com</h3>
+
+                    </div>
                 </div>
             </div>
-        </div>
+            
+        
     )
 }
 
